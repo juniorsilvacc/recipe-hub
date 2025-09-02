8f67a9d085a51d9b15188d7192f3567ed3e9c1b4
juniorsilvaneg <juniorsilvacc@hotmail.com>
2025-09-02 14:29:50 -0300

    configuração de upload de imagens e statics

--- app/settings.py
+++ app/settings.py
@@ -121,12 +121,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+
 STATIC_URL = 'static/'
 
-# Configurr arquivos estáticos
+# Junta todos os arquivos estáticos em STATIC_ROOT
+# Depois, configure o servidor (Nginx/Apache) para servir essa pasta
 STATICFILES_DIRS = [
-    BASE_DIR / 'app/static'
-] # Diretórios adicionais para arquivos estáticos
+    BASE_DIR / 'app/static' # Pastas adicionais com arquivos estáticos do projeto
+]
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media' # Pasta onde arquivos enviados pelo usuário serão salvos
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls'))
 ]
+
+# ⚠️ Apenas para desenvolvimento! 
+# Em produção, use Nginx/Apache ou serviços como S3/GCP para servir arquivos.
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Uploads de usuários
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # CSS, JS, imagens
